051a6f9efca8018d712a913e2e951d7223fc95c2
evertcode <hebert.hernandez7@gmail.com>
2016-11-29 22:13:01 -0600

    route /setup

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-let express = require('express');
-let app = express();
-let mongoose = require('mongoose');
-let morgan = require('morgan');
-let bodyParser = require('body-parser');
-let port = 3000;
-let book = require('./controllers/routes/book');
-let config = require('config');
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var port = 3000;
+var book = require('./controllers/routes/book');
+var config = require('config');
 
-let options = {
+var options = {
     server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
     replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
 };
@@ -15,7 +15,7 @@ let options = {
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DBHost, options);
-let db = mongoose.connection;
+var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 
@@ -24,12 +24,29 @@ if (config.util.getEnv('NODE_ENV') !== 'test') {
 }
 
 
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json' }));
 
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+    if (req.method == 'OPTIONS') {
+        res.status(200).end();
+    } else {
+        next();
+    }
+});
+
 app.get("/", (req, res) => res.json({ message: "Welcome to our Bookstore!" }));
+
+app.route("/setup")
+    .get(book.setup);
 
 app.route("/book")
     .get(book.getBooks)
--- controllers/routes/book.js
+++ controllers/routes/book.js
@@ -1,9 +1,9 @@
-let mongoose = require('mongoose');
-let Book = require('../models/book');
+var mongoose = require('mongoose');
+var Book = require('../models/book');
 
 
 function getBooks(req, res) {
-    let query = Book.find({});
+    var query = Book.find({});
     query.exec((err, books) => {
         if (err) res.send(err);
         res.json(books);
@@ -36,7 +36,7 @@ function getBook(req, res) {
 
 function deleteBook(req, res) {
     Book.remove({ _id: req.params.id }, (err, result) => {
-        res.json({ message: "Book successfully deleted!", result });
+        res.json({ message: "Book successfully devared!", result });
     });
 }
 
@@ -52,4 +52,51 @@ function updateBook(req, res) {
 }
 
 
-module.exports = { getBooks, postBook, getBook, deleteBook, updateBook };
+function setup(req, res) {
+    //var newBook = new Book(req.body);
+    var books = [
+        {
+            title: 'Poema de Gilgamesh',
+            author: 'Anónimo',
+            year: '-600',
+            pages: '1000'
+        },
+        {
+            title: 'Las mil y una noche',
+            author: 'Anónimo',
+            year: '700',
+            pages: '1587'
+        },
+        {
+            title: 'Saga de Njál',
+            author: 'Anónimo',
+            year: '700',
+            pages: '789'
+        },
+        {
+            title: 'Todo se desmorona',
+            author: 'Chinua Achebe',
+            year: '1958',
+            pages: '968'
+        },
+        {
+            title: 'Divina comedia',
+            author: 'Dante Alighieri',
+            year: '1265',
+            pages: '1878'
+        }
+    ];
+
+
+    for (var i = 0; i < books.length; i++) {
+        new Book(books[i]).save(function (err) {
+            if (err) throw err;
+            console.log('Book  saved successfully')
+        });
+    }
+
+    res.json({ success: true });
+
+}
+
+module.exports = { getBooks, postBook, getBook, deleteBook, updateBook, setup };
